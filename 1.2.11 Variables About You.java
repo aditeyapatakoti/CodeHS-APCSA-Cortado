060c5ac7a79a1cb44e4c84cf044b8ab1ae69ba99
iFront <83433323+aditeyapatakoti@users.noreply.github.com>
2025-08-28 10:36:33 -0400

    Create 1.2.11 Variables About You.java

---
+++ 1.2.11 Variables About You.java
@@ -0,0 +1,26 @@
+public class Variables 
+{
+    public static void main(String[] args)
+    {
+        // 1. Declare and initialize a variable for each scenario:
+        
+        // A friend's name
+        String friend = "Bob";
+        // Your lucky number
+        int luckyNum = 4;
+        // Whether or not you are you a morning person (the value 
+        // should be either `true` or `false`)
+        boolean morningPerson = false;
+        // Your favorite snack
+        String snack = "chips";
+        // The price of your favorite snack
+        double price = 2.99;
+        
+        // 2. Print out each variable:
+        System.out.println(friend);
+        System.out.println(luckyNum);
+        System.out.println(morningPerson);
+        System.out.println(snack);
+        System.out.println(price);
+    }
+}
