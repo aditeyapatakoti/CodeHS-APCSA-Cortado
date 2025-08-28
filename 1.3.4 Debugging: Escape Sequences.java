56d2fe4f7aacc1c4c469acfb89836ca0097dfee8
iFront <83433323+aditeyapatakoti@users.noreply.github.com>
2025-08-28 10:38:59 -0400

    Create 1.3.4 Debugging: Escape Sequences.java

---
+++ 1.3.4 Debugging: Escape Sequences.java
@@ -0,0 +1,8 @@
+public class BuggyEscapes
+{
+    public static void main(String[] args)
+    {
+        System.out.print("\"Make it work, \nmake it right, \nmake it fast.\"");
+        System.out.println("\n\t-Kent Beck");
+    }
+}
