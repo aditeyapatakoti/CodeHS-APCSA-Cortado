19d5daaa77a19ed44f7c1bb12cdea5b4cce00282
iFront <83433323+aditeyapatakoti@users.noreply.github.com>
2025-08-28 10:35:57 -0400

    Create 1.2.10 Signature with Strings.java

---
+++ 1.2.10 Signature with Strings.java
@@ -0,0 +1,14 @@
+public class Signature
+{
+    public static void main(String[] args)
+    {
+        String firstName = "John";
+        String lastName = "Doe";
+        String date = "August 28, 2025";
+        System.out.println("Name: ");
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println("Date: ");
+        System.out.println(date);
+    }
+}
