9bc91353c7789a9df17eeb4fca202625d66e7f70
iFront <83433323+aditeyapatakoti@users.noreply.github.com>
2025-08-28 10:30:33 -0400

    Create 1.2.4 Exploration: Social Media Variables.java

---
+++ 1.2.4 Exploration: Social Media Variables.java
@@ -0,0 +1,31 @@
+public class SocialMediaVariables 
+{
+
+    public static void main(String[] args)
+    {
+        int snapchatsSent = 353;
+        System.out.println("Number of snapchats sent: ");
+        System.out.println(snapchatsSent);
+        
+        double youTubeVideosWatched = 130.5;
+        System.out.println("Number of YouTube videos watched: ");
+        System.out.println(youTubeVideosWatched);
+        
+        int instagramPosts = 0;
+        System.out.println("Number of Instagram posts: ");
+        System.out.println(instagramPosts);
+        // Data updated two weeks later
+        snapchatsSent = 400;
+        System.out.println("Number of snapchats sent: ");
+        System.out.println(snapchatsSent);
+        
+        youTubeVideosWatched = 240.4;
+        System.out.println("Number of YouTube videos watched: ");
+        System.out.println(youTubeVideosWatched);
+        
+        instagramPosts = 10;
+        System.out.println("Number of Instagram posts: ");
+        System.out.println(instagramPosts);
+    }
+
+}
