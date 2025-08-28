f3b0268eddeb5d4909d70d4b6cdd533b2d094d9b
iFront <83433323+aditeyapatakoti@users.noreply.github.com>
2025-08-28 10:40:54 -0400

    Create 1.3.6 Strong Passwords Formatting.java

---
+++ 1.3.6 Strong Passwords Formatting.java
@@ -0,0 +1,9 @@
+public class StrongPasswords
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Length\nThe longer a password is, the more secure it is.");
+        System.out.print("\n\nComplex Characters\nStrong passwords include a mix of uppercase and lowercase letters, numbers, and special characters.");
+        System.out.print("\n\nUnpredictability\nPasswords should avoid using common words, phrases, and per sonal information.");
+    }
+}
